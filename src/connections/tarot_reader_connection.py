68ad770d43d44668f415c62917816d56c9a634a6
Uriel Romeo <uromeo.soc@gmail.com>
2025-02-18 16:36:06 +0100

    improved dall-e prompt

--- src/connections/tarot_reader_connection.py
+++ src/connections/tarot_reader_connection.py
@@ -388,6 +388,7 @@ You will improve this text in order to create a dall-e prompt
 * A tarot card illustration in the Rider-Waite style, featuring a [add something here ], symbolizing [add something here ]. The figure wears [ add something here ], and [ add something here ]. A [add something here ]. The background is [add something here ], evoking [add something here ]. The illustration is hand-drawn with bold black outlines, vibrant flat colors, and subtle shading to create depth,  staying true to the classic tarot aesthetic *
 You will add a character description based on the following
 Add negative parameters to including text in the image, and we want only one card
+Below is the mystical reading, you'll fill in the blanks with the mystical reading, but you will not include the text below in your output.
 { mystical_reading }
                 """
                 dalle_friendly_prompt = openai_conn.perform_action("generate-text", { "prompt": dalle_friendly_prompt_content, "system_prompt": system_prompt })
